7b66c38f74bfae3f1c8e2dc33c9f1fdc0a254e8d
huypham <hoanghuy7284022@gmail.com>
2024-04-06 20:27:53 +0700

    commit

---
+++ src/gamecontroller.js
@@ -0,0 +1,10 @@
+var startGame
+
+$(document).ready(()=>{
+    startGame =  function(){
+        let gameScreen = $('.game-wrapper__game-screen')
+        let gameMenu = $('.game-wrapper__game-screen__game-menu')
+        alert(gameScreen)
+       
+    }
+})
